5e92eef9457653492dfcc95b2e015fa3a857197b
CHO JEONGHWAN <kyjjhh1@gmail.com>
2024-11-14 01:00:24 +0900

    initial commit

---
+++ data_utils/ModelNetDataLoader.py
@@ -0,0 +1,168 @@
+'''
+@author: Xu Yan
+@file: ModelNet.py
+@time: 2021/3/19 15:51
+'''
+import os
+import numpy as np
+import warnings
+import pickle
+import pandas as pd
+
+from tqdm import tqdm
+from torch.utils.data import Dataset
+
+warnings.filterwarnings('ignore')
+
+
+def pc_normalize(pc):
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc**2, axis=1)))
+    pc = pc / m
+    return pc
+
+
+def farthest_point_sample(point, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [N, D]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [npoint, D]
+    """
+    N, D = point.shape
+    xyz = point[:,:3]
+    centroids = np.zeros((npoint,))
+    distance = np.ones((N,)) * 1e10
+    farthest = np.random.randint(0, N)
+    for i in range(npoint):
+        centroids[i] = farthest
+        centroid = xyz[farthest, :]
+        dist = np.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = np.argmax(distance, -1)
+    point = point[centroids.astype(np.int32)]
+    return point
+
+def load_off_vertices(file_path):
+    with open(file_path, 'r') as f:
+        # 첫 번째 줄은 'OFF' 문자열이므로 건너뜀
+        first_row = f.readline().strip()
+        if first_row != 'OFF':
+            n_verts, n_faces, _ = map(int, first_row.lstrip('OFF').strip().split())
+        
+        # 두 번째 줄에서 정점과 면의 개수 읽기
+        else:
+            n_verts, n_faces, _ = map(int, f.readline().strip().split())
+            
+        # 정점 좌표 읽기
+        vertices = [list(map(float, f.readline().strip().split())) for _ in range(n_verts)]
+    return np.array(vertices, dtype=np.float32)
+
+def pad__points(point_set, target_npoints=1024):
+    num_points = point_set.shape[0]
+    
+    additional_points = np.random.choice(num_points, target_npoints - num_points, replace=True)
+    point_set = np.vstack([point_set, point_set[additional_points]])
+
+    return point_set
+
+class ModelNetDataLoader(Dataset):
+    def __init__(self, root, args, split='train', process_data=False):
+        self.root = root
+        # self.npoints = args.num_point
+        self.npoints = 1024
+        self.process_data = process_data
+        # self.uniform = args.use_uniform_sample
+        self.uniform = False
+        # self.use_normals = args.use_normals
+        self.use_normals = False
+        # self.num_category = args.num_category
+        self.num_category = 40
+
+        shape_ids = {}
+
+        # 각 ids에 리스트 형식으로 ['airplane', 'airplane_0640.off'] 저장
+        shape_ids['train'] = [[(line.strip().split(','))[1],(line.strip().split(','))[3]] for line in open(os.path.join(self.root, 'metadata_modelnet40.csv')) if 'train' in line]
+        shape_ids['test'] = [[(line.strip().split(','))[1],(line.strip().split(','))[3]] for line in open(os.path.join(self.root, 'metadata_modelnet40.csv')) if 'test' in line]
+
+        self.classes = sorted(list(set([(line.strip().split(','))[1] for line in open(os.path.join(self.root, 'metadata_modelnet40.csv')) if 'test' in line])))
+
+        assert (split == 'train' or split == 'test') # False 뜨면 에러 뽑고 중단.
+
+        self.datapath = [ids for ids in shape_ids[split]]
+
+        if self.uniform:
+            self.save_path = os.path.join(root, 'modelnet%d_%s_%dpts_fps.dat' % (self.num_category, split, self.npoints))
+        else:
+            self.save_path = os.path.join(root, 'modelnet%d_%s_%dpts.dat' % (self.num_category, split, self.npoints))
+
+        if self.process_data:
+            if not os.path.exists(self.save_path):
+                self.list_of_points = [None] * len(self.datapath)
+                self.list_of_labels = [None] * len(self.datapath)
+
+                for index in tqdm(range(len(self.datapath)), total=len(self.datapath)):
+                    cls = self.datapath[index][0]
+                    cls = self.classes.index(cls)
+
+                    point_set = load_off_vertices(os.path.join(self.root, self.datapath[index][1]))
+
+                    if self.uniform:
+                        point_set = farthest_point_sample(point_set, self.npoints)
+                    else:
+                        point_set = point_set[0:self.npoints, :]
+
+                    self.list_of_points[index] = point_set
+                    self.list_of_labels[index] = cls
+
+                with open(self.save_path, 'wb') as f:
+                    pickle.dump([self.list_of_points, self.list_of_labels], f)
+            else:
+                print('Load processed data from %s...' % self.save_path)
+                with open(self.save_path, 'rb') as f:
+                    self.list_of_points, self.list_of_labels = pickle.load(f)
+
+    def __len__(self):
+        return len(self.datapath)
+
+    def _get_item(self, index):
+        if self.process_data:
+            point_set, label = self.list_of_points[index], self.list_of_labels[index]
+
+        else:
+            label = self.datapath[index][0]
+            label = self.classes.index(label)
+
+            point_set = load_off_vertices(os.path.join(self.root, self.datapath[index][1]))
+
+
+            if self.uniform:
+                point_set = farthest_point_sample(point_set, self.npoints)
+            else:
+                point_set = point_set[0:self.npoints, :]
+                
+        point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
+        if not self.use_normals:
+            point_set = point_set[:, 0:3]
+
+        if point_set.shape[0] < self.npoints:
+            point_set = pad__points(point_set, self.npoints)
+
+        return point_set, label
+
+    def __getitem__(self, index):
+        return self._get_item(index)
+
+
+if __name__ == '__main__':
+    import torch
+    import pandas as pd
+
+    data = ModelNetDataLoader('./data/modelnet40_normal_resampled/', split='train', args=' ')
+    DataLoader = torch.utils.data.DataLoader(data, batch_size=12, shuffle=True)
+    for point, label in DataLoader:
+        print(point.shape)
+        print(label.shape)
